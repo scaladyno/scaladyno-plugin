cd74e109d6413c294ea46a7da7783814390b8a69
CedricBastin <cedricbastin@gmail.com>
2014-04-04 17:25:17 +0200

    Plugin hacking

--- components/plugin/src/dyno/plugin/transform/prepare/DynoPrepareTreeTransformer.scala
+++ components/plugin/src/dyno/plugin/transform/prepare/DynoPrepareTreeTransformer.scala
@@ -12,28 +12,19 @@ trait DynoPrepareTreeTransformer {
   import helper._
 
   class TreePreparer(unit: CompilationUnit) extends TypingTransformer(unit) {
-    override def transform(tree: Tree): Tree = {
-        //debug
-        tree match {
-          case x:TermTree =>
-            println("prep: TermTree")
-
-          case x:TypTree =>
-            println("prep: TypTree")
-            // first only treat term trees
-          case x =>
-            println("stm else")
-        }
-        tree match {
-    //        // remove redundant asInstanceOf-s introduced by tailcalls
-    //      case AsInstanceOf(expr, tpe) if expr.tpe =:= tpe.tpe && tpe.tpe.typeSymbol.isDynoClass =>
-    //        dynolog("removed redundant asInstanceOf:")
-    //        dynolog("  tree: " + tree)
-    //        dynolog("  expr: " + tree)
-    //        dynolog("  tree.tpe: " + tree.tpe)
-    //        dynolog("  expr.tpe: " + expr.tpe)
-    //        dynolog("  tpe.tpe: " + tpe.tpe)
-    //        transform(expr)
+    override def transform(tree: Tree): Tree = { //[T <: Tree]
+      //debug:
+      println("prep: " + tree.getClass + " tpe: "+ tree.tpe + " tree: " +tree)
+      tree match {
+        //        // remove redundant asInstanceOf-s introduced by tailcalls
+        //      case AsInstanceOf(expr, tpe) if expr.tpe =:= tpe.tpe && tpe.tpe.typeSymbol.isDynoClass =>
+        //        dynolog("removed redundant asInstanceOf:")
+        //        dynolog("  tree: " + tree)
+        //        dynolog("  expr: " + tree)
+        //        dynolog("  tree.tpe: " + tree.tpe)
+        //        dynolog("  expr.tpe: " + expr.tpe)
+        //        dynolog("  tpe.tpe: " + tpe.tpe)
+        //        transform(expr)
 
         //Tree
           //trait TermTree extends Tree
@@ -86,22 +77,62 @@ trait DynoPrepareTreeTransformer {
             //case class Return(expr: Tree) extends SymTree with TermTree
             //case class ApplyDynamic(qual: Tree, args: List[Tree]) extends SymTree with TermTree
             //case class This(qual: TypeName) extends SymTree with TermTree
-          //trait NameTree extends Tree => extends RefTree and DefTree
+          //trait NameTree extends Tree
+          //  => extends RefTree and DefTree
           //case class CaseDef(pat: Tree, guard: Tree, body: Tree)
-          //case classAnnotated(annot: Tree, arg: Tree)
-                //case ClassDef(mods, name, tparams, impl) =>//(mods: Modifiers, name: TypeName, tparams: List[TypeDef], impl: Template)
-            
-          /*case b@Block(stats, result) =>
+          //case class Annotated(annot: Tree, arg: Tree)
+
+
+        /*case b@Block(stats, result) =>
           println("my case")
-            val stats2 = stats.filter(_.tpe.isErroneous)
-            b
-            //localTyper.typed(Block(stats2, result))*/
-          case x =>
-            println("check")
-            x
-            //super.transform(tree)
-        }
+          val stats2 = stats.filter(_.tpe.isErroneous)
+          b
+          //localTyper.typed(Block(stats2, result))*/
+
+          // ==> traverser : wa
+
+        case x if x.tpe.isErroneous =>
+          //tpe is a field of all trees => it is a Type
+          //iserroneous can be applied to any tree through TreeContextApiImpl which is implemented by tree
+          x match {
+            case _: TermTree => println("erroneous TermTree => replaced by EmptyTree: "+x); EmptyTree
+            case _: TypTree => println("isErroneous Typtree: "+x); x
+            case _: CaseDef => println("case class CaseDef..."); x
+            case _: Annotated => println("case class Annotated..."); x
+          }
+          println(x+" is isErroneous")
+          //TypeTree()
+          EmptyTree//.asInstanceOf[T]
+        case PackageDef(pid: RefTree, stats: List[Tree]) =>
+          PackageDef(pid, stats.map(transform(_)))
+        case ModuleDef(mods, name, impl) =>
+          //Template(parents: List[Tree], self: ValDef, body: List[Tree])
+          ModuleDef(mods, name, Template(impl.parents.map(transform(_)), impl.self, impl.body.map(transform(_))))
+        case DefDef(mods, name, tparams, vparamss: List[List[ValDef]], tpt: Tree, rhs: Tree) =>
+          if (rhs.isErroneous || rhs.tpe.isErroneous || tpt.isErroneous || tpt.tpe.isErroneous) {
+            println("replace DefDef with empty")
+            EmptyTree
+          }
+          DefDef(mods, name, tparams.map(transform(_).asInstanceOf[TypeDef]), vparamss.map(_.map(transform(_).asInstanceOf[ValDef])), transform(tpt), transform(rhs))
+        /*case c @ ClassDef(mods, name, tparams, impl) =>//(mods: Modifiers, name: TypeName, tparams: List[TypeDef], impl: Template)
+          println("class def:")
+          c*/
+        case Block(stats, expr) =>
+          Block(stats.map(transform(_)), transform(expr))
+        case ValDef(mods, name, tpt, rhs) =>//ValdDef(mods: Modifiers, name: TermName, tpt: Tree, rhs: Tree)
+          if (rhs.isErroneous || rhs.tpe.isErroneous || tpt.isErroneous || tpt.tpe.isErroneous) {
+            println("replace ValDef with empty")
+            return Literal(Constant(()))//Block(Nil, Literal(Constant(())))
+          }
+
+          val caca = ValDef(mods, name, transform(tpt), transform(rhs))
+          println("ValDef done tpt:"+ tpt + " rhs: " + rhs )
+          caca
+        case x => //catch all
+          println("last check: "+x+" of type: "+x.tpe)
+          x
+          //super.transform(tree)
       }
-      
+    }  
   }
 }
