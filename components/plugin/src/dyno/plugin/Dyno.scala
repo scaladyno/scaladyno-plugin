5c8a0b07866957d8e5d1c1e7814f8647d819ecf3
Vlad Ureche <vlad.ureche@gmail.com>
2014-04-08 23:01:23 +0200

    Leftovers

--- components/plugin/src/dyno/plugin/Dyno.scala
+++ components/plugin/src/dyno/plugin/Dyno.scala
@@ -27,15 +27,15 @@ class Dyno(val global: Global) extends Plugin { plugin =>
   class OurHackedReporter(orig: Reporter) extends Reporter {
     val super_info0 = orig.getClass.getMethod("info0", classOf[Position], classOf[String], classOf[Severity], classOf[Boolean])
     def info0(pos: Position, msg: String, severity: Severity, force: Boolean): Unit = {
-      val super_severity = severity match {
+      val (super_severity, super_msg) = severity match {
         case ERROR =>
           // put in map
-          orig.WARNING
-        case WARNING => orig.WARNING
-        case INFO => orig.INFO
-        case _ => orig.INFO
+          (orig.WARNING, "[suppressed error] " + msg)
+        case WARNING => (orig.WARNING, msg)
+        case INFO => (orig.INFO, msg)
+        case _ => (orig.INFO, msg)
       }
-      super_info0.invoke(orig, pos, msg, super_severity, force.asInstanceOf[AnyRef])
+      super_info0.invoke(orig, pos, super_msg, super_severity, force.asInstanceOf[AnyRef])
     }
   }
 
