17c14856afbb5bb781a4a49efa32158264a53c71
CedricBastin <cedricbastin@gmail.com>
2014-04-11 10:56:55 +0200

    more cases

--- components/plugin/src/dyno/plugin/transform/prepare/DynoPrepareTreeTransformer.scala
+++ components/plugin/src/dyno/plugin/transform/prepare/DynoPrepareTreeTransformer.scala
@@ -16,19 +16,28 @@ trait DynoPrepareTreeTransformer {
       //debug:
       //println("prep: " + tree.getClass + " tpe: "+ tree.tpe + " tree: " +tree)
       tree match {
-        //tpe is a field of all trees => it is a Type
+        //tpe is a field of all trees => it is a "Type"
         //isErroneous can be applied to any tree through TreeContextApiImpl which is implemented by tree
-
-        case ValDef(mods, name, tpt, rhs) if (rhs.isErroneous || rhs.tpe.isErroneous || tpt.isErroneous || tpt.tpe.isErroneous) =>//ValdDef(mods: Modifiers, name: TermName, tpt: Tree, rhs: Tree)
-           EmptyTree
+        //  def isErroneous = (tpe ne null) && tpe.isErroneous
+        //  def isErrorTyped = (tpe ne null) && tpe.isError -> not recursive
+        case ValDef(mods, name, tpt, rhs) if (tpt.isErroneous || rhs.isErroneous) =>
+          EmptyTree
+        case Assign(lhs, rhs) if (lhs.isErroneous || rhs.isErroneous)=>
+          EmptyTree
         case Block(stmts, expr) =>
           val expr1 = if (expr.isErroneous) gen.mkAttributedIdent(Predef_???) else expr
           val tree1 = Block(stmts.filterNot(x => x.isErroneous), expr1)
           val tree2 = localTyper.typed(tree1)
           val tree3 = super.transform(tree2)
           tree3
+        case Select(qualif, _) if (qualif.isErroneous) =>
+          EmptyTree
         case expr if expr.isErroneous =>
           localTyper.typed(gen.mkAttributedIdent(Predef_???))
+        case x if x.isErroneous =>
+          EmptyTree
+          //this.
+          //treeCopy.Block(tree, transformStats(stats.filter(!_.tpe.isErroneous), currentOwner), transform(expr))
         case x =>
           super.transform(x)
       }
